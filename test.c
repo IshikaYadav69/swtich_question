a1004d5da7e22be0057bb89f158da79331a503ea
unknown <ishika.yadav3@s.amity.edu>
2025-07-20 21:35:14 -0700

    C4

--- test.c
+++ test.c
@@ -9,4 +9,9 @@ int main(void) {
 
     printf("Enter two numbers: ");
     if (scanf("%lf %lf", &num1, &num2) != 2) return 1;
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            printf("%.2lf %c %.2lf = %.2lf\n", num1, op, num2, result);
+            break;
 
