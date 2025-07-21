fe5184265a8c15cbe3ab9b07004e228892b1136e
unknown <ishika.yadav3@s.amity.edu>
2025-07-20 21:36:57 -0700

    C5

--- test.c
+++ test.c
@@ -14,4 +14,8 @@ int main(void) {
             result = num1 + num2;
             printf("%.2lf %c %.2lf = %.2lf\n", num1, op, num2, result);
             break;
+	case '-':
+            result = num1 - num2;
+            printf("%.2lf %c %.2lf = %.2lf\n", num1, op, num2, result);
+            break;
 
