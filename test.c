f62ad6bd8be80f339c1b1a052748b70202b03c0e
unknown <ishika.yadav3@s.amity.edu>
2025-07-20 21:34:29 -0700

    C3

--- test.c
+++ test.c
@@ -4,5 +4,9 @@ int main(void) {
     char op;
     double num1, num2, result;
 
-    // User input
     printf("Enter an operator (+, -, *, /): ");
+    if (scanf(" %c", &op) != 1) return 1;
+
+    printf("Enter two numbers: ");
+    if (scanf("%lf %lf", &num1, &num2) != 2) return 1;
+
